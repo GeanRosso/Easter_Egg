cf93ac69f71ea8e4b63595375962838ab38b7063
Geanrosso <geancls226@gmail.com>
2026-04-07 22:21:14 +0200

    webpai

--- WebApi_FrontEnd/WebAPI/WebAPI/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
+++ WebApi_FrontEnd/WebAPI/WebAPI/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+770d76c73506a59d3e3fa213864c362608d8d38d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
